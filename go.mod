453cba98624a70112449990a68d432def8ecd5c9
amirrezaask <raskarpour@gmail.com>
2021-01-14 10:39:58 +0330

    add go.mod, go.sum, add logger interface, add logrus impl, use flags to pass options

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/2hamed/httplogserver
+
+go 1.15
+
+require github.com/sirupsen/logrus v1.7.0
--- main.go
+++ main.go
@@ -1,62 +1,72 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"io/ioutil"
 	"net"
 	"net/http"
-	"strings"
+	"os"
 )
-
-var host = "0.0.0.0"
-var port = 8000
-
+var host string
+var port int
+var indentLength int
+var logger string
+	
+func parseFlags() {
+	flag.StringVar(&host, "host", "0.0.0.0", "Host you want to run on, by default 0.0.0.0")
+	flag.IntVar(&port, "port", 8000, "port you want to expose")
+	flag.IntVar(&indentLength, "indent_length", 5, "Indent you want for nested values.")
+	flag.StringVar(&logger, "logger", "logrus", "Logger library you want to use")
+	flag.Parse()
+}
 func main() {
+	parseFlags()
+	logger := NewLogger(logger)
 	listen, err := net.Listen("tcp", fmt.Sprintf("%s:%d", host, port))
 	if err != nil {
-		panic(err)
+		logger.Printf("error starting tcp listener: %v\n", err)
+		os.Exit(1)
 	}
 	server := &http.Server{
-		Handler: handler{},
+		Handler: handler{logger},
 	}
-	fmt.Printf("Starting HTTP server on %s:%d", host, port)
+	logger.Printf("Starting HTTP server on %s:%d", host, port)
+
 	if err := server.Serve(listen); err != nil {
-		panic(err)
+		logger.Printf("error in serving handler with tcp server: %v\n", err)
 	}
 }
 
 type handler struct {
+	logger Logger
 }
 
 func (h handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("\n/-------------- HTTP request received --------------/")
-	fmt.Printf("From: %s\n", r.RemoteAddr)
-	fmt.Printf("Request URI: %s\n", r.RequestURI)
-	fmt.Printf("Method: %s\n", r.Method)
+	h.logger.Printf("\n/-------------- HTTP request received --------------/\n")
+	h.logger.Printf("From: %s\n", r.RemoteAddr)
+	h.logger.Printf("Request URI: %s\n", r.RequestURI)
+	h.logger.Printf("Method: %s\n", r.Method)
 
-	fmt.Printf("\nHeaders:\n")
-	ValuesPrettyPrint(5, r.Header)
+	h.logger.Printf("\nHeaders:\n")
+	valuesPrettyPrint(5, r.Header)
 
-	fmt.Printf("\nQuery Params:\n")
-	ValuesPrettyPrint(5, r.URL.Query())
+	h.logger.Printf("\nQuery Params:\n")
+	valuesPrettyPrint(5, r.URL.Query())
 
 	r.ParseForm()
-	fmt.Printf("\nPost Params:\n")
-	ValuesPrettyPrint(5, r.Form)
+	h.logger.Printf("\nPost Params:\n")
+	valuesPrettyPrint(5, r.Form)
 
 	body, err := ioutil.ReadAll(r.Body)
 	if err != nil {
-		fmt.Println("Error reading request body:", err)
+		h.logger.Printf("Error reading request body:%v\n", err)
 	}
 	defer r.Body.Close()
 
-	fmt.Printf("\nRAW Body:\n %s", body)
+	h.logger.Printf("\nRAW Body:\n %s\n", body)
 
-	fmt.Println("\n/-------------- END --------------/\n")
+	h.logger.Printf("\n/-------------- END --------------/\n")
 }
 
-func ValuesPrettyPrint(indentCount int, values map[string][]string) {
-	for k, v := range values {
-		fmt.Printf("%s %s => %v\n", strings.Repeat(" ", indentCount), k, v)
-	}
-}
+
